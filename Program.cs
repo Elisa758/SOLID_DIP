f43b902daae56fa2d2032cc57ccde9a9d51554b9
Elisa758 <57760897+Elisa758@users.noreply.github.com>
2020-02-11 14:58:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace SOLID_DIP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Terminal terminal = new Terminal();
+            while (!terminal.Exited)
+            {
+                ICommand command = terminal.PromptCommand();
+                terminal.ExecuteCommand(command);
+            }
+        }
+    }
+
+}
---
+++ Terminal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_DIP
+{
+    public class Terminal
+    {
+        public bool Exited { get; set; }
+        private string _promptString;
+
+        public Terminal()
+        {
+            _promptString = String.Format("{0}$ ", System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+            Exited = false;
+        }
+
+        public ICommand PromptCommand()
+        {
+            string commandLine = Prompt();
+            return new Command(commandLine);
+        }
+
+        public string Prompt()
+        {
+            Console.Write(_promptString);
+            string userCommand = Console.ReadLine();
+            return userCommand;
+        }
+
+        public void ExecuteCommand(ICommand command)
+        {
+            try
+            {
+                command.Launch();
+                if (command.Output.Length > 0)
+                {
+                    Console.WriteLine(command.Output);
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                Console.Error.WriteLine("{0}: path not found", command);
+            }
+        }
+    }
+}
